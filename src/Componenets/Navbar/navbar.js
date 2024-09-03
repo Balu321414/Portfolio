7e5624065ae1e0a8dbade8ce4145459f5c1bfb54
Balu321414 <balu3214aa@gmail.com>
2024-09-03 15:55:59 +0530

    updated navbar

--- src/Componenets/Navbar/navbar.js
+++ src/Componenets/Navbar/navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-       <div className="scrolling-text">Some text here</div>
+       <div className="scrolling-text"><span color="red">D</span>eveloper</div>
             <div className="desktopMenu" style={{ display: 'flex' }}>
                 <Link activeClass='active' to='introduction' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
                 <Link activeClass='active' to='about' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">About</Link>
